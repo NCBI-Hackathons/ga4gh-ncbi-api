7fb7ea1156f59bf15447e4cf2145369085434883
Alexander Wong <wongak626@gmail.com>
2017-03-21 16:03:41 -0400

    Made functional flask program

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from ga4gh.schemas import protocol
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-	return render_template('index.html')
+#@app.route('/')
+#def index():
+#	return render_template('index.html')
 
 def get_datasets(request):
 	"""
@@ -15,27 +15,20 @@ def get_datasets(request):
 	dataset_list = []
 	for i in xrange(10):
 		dataset = protocol.Dataset()
-		dataset.id = i
+		dataset.id = str(i)
 		dataset.name = "Hi there"
 		dataset_list.append(dataset)
 	return dataset_list
 
 @app.route('/datasets/search', methods=['POST'])
-def search_datasets():
+def Search_Datasets():
 	search_request = protocol.SearchDatasetsRequest
 	deserialized_request = protocol.fromJson(request.get_data(), search_request)
-	print(deserialized_request.page_token)
-	print(deserialized_request.page_size)
-	# dataset_list = get_datasets(deserialized_request)
-	# response_proto = protocol.SearchDatasetsResponse()
-	# reponse_proto.extend(dataset_list)
-	# return protocol.toJson(response_proto)
-	# SearchDatasetsRequest = json.loads(SearchDataSetsRequest)
-	return json.jsonify(request.get_json())
+	dataset_list = get_datasets(deserialized_request)
+	response_proto = protocol.SearchDatasetsResponse()
+	response_proto.datasets.extend(dataset_list)
+	return protocol.toJson(response_proto)
 
-# @app.route('/datasets/<dataset_id>', methods = ['GET'])
-# def GetDataset(request):
-# 	return Dataset
-	
+
 if __name__ == '__main__':
 	app.run(debug = True)
